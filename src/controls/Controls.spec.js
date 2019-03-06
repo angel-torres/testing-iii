64c4660399f05a7b0b872984a91d7614b1d6a32b
Angel Torres <alas4224@gmail.com>
2019-03-06 16:36:56 -0600

    add 4 tests to controls test file

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,46 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+
+import Controls from './Controls';
+
+describe('<Controls/>', () => {
+    it('toggleClose function should be called', () => {
+        const toggleClose = jest.fn()
+
+        const { getByText } = render(<Controls toggleClosed={toggleClose}/>)
+
+        const closeGateBtn = getByText(/close gate/i);
+
+        fireEvent.click(closeGateBtn)
+
+        expect(toggleClose).toHaveBeenCalled()
+    })
+
+
+    it('toggleClose function should be called', () => {
+        const toggleLocked = jest.fn();
+        const closed = true;
+
+        const { getByText } = render(<Controls closed={true} toggleLocked={toggleLocked}/>)
+
+        const lockGateBtn = getByText(/lock gate/i);
+
+        fireEvent.click(lockGateBtn);
+
+        expect(toggleLocked).toHaveBeenCalled()
+    })
+
+    it('should render lock gate button', () => {
+        const { getByText } = render(<Controls/>) 
+
+        expect(getByText(/lock gate/i)).toBeInTheDocument()
+    })
+
+    it('should render close gate button', () => {
+        const { getByText } = render(<Controls/>) 
+
+        expect(getByText(/close gate/i)).toBeInTheDocument()
+    })
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect'
+
+import Dashboards from './Dashboard';
+
+describe('<Dashboard/>', () => {
+    // it('toggleClose function should be called', () => {
+    //     const toggleClose = jest.fn()
+
+    //     const { getByText } = render(<Dashboards toggleClosed={toggleClose}/>)
+
+    //     const closeGateBtn = getByText(/close gate/i);
+
+    //     fireEvent.click(closeGateBtn)
+
+    //     expect(toggleClose).toHaveBeenCalled()
+    // })
+})
+
+
+
 // Test away
